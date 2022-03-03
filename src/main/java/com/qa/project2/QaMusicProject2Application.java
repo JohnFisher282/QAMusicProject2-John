ce01af4a99a93fc6dbb09176a7dab9784a727e24
JohnFisher282 <johnfisher282@gmail.com>
2022-03-03 14:20:16 +0000

    initial commit

---
+++ src/main/java/com/qa/project2/QaMusicProject2Application.java
@@ -0,0 +1,13 @@
+package com.qa.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaMusicProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaMusicProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/project2/QaMusicProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaMusicProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
